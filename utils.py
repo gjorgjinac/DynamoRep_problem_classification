c81fd120f3b46c63dee0a1863560c0b0a5fec3e6
Gjorgjina Cenikj <gjorgjinac@gmail.com>
2023-02-03 10:40:37 +0000

    add method to utils file

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from algorithm_baseline_utils import *
 from run_utils import *
 from run_utils import *
 from sklearn.ensemble import RandomForestClassifier,RandomForestRegressor
@@ -185,3 +184,10 @@ def difference_features(feature_df,max_iteration):
     #differenced_features['seed']=feature_df.reset_index()['seed'].values
     return differenced_features
 
+
+
+def get_argument_elements_from_list(argument_list_str, cast_to_int=False):
+    if cast_to_int:
+        return [int(a) for a in argument_list_str.split('-')] if '-' in argument_list_str else [int(argument_list_str)]
+    else:
+        return argument_list_str.split('-') if '-' in argument_list_str else [argument_list_str]
